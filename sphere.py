668bec9df68b2e549f2c9d46ca1b76a327628975
Yuanguo-notebook <45431499+Yuanguo-notebook@users.noreply.github.com>
2019-11-27 19:49:57 -0500

    Add files via upload

--- sphere.py
+++ sphere.py
@@ -2,46 +2,42 @@
 
 from sphere_to_cubemap.geom3 import Vector3, Point3, Ray3, dot, unit
 from math import sqrt
-from sphere_to_cubemap.hit import Hit
+
 
 class Sphere(object):
-    """A ray-traceable sphere"""
+
     
-    def __init__(self, centre, radius, material):
-        """Create a sphere with a given centre point, radius
-        and surface material"""
+    def __init__(self, centre, radius):
+
         self.centre = centre
         self.radius = radius
-        self.material = material
-
-
-    def normal(self, p):
-        """The surface normal at the given point on the sphere"""
-        return unit(p - self.centre)
 
     def intersect(self, ray):
-        """The ray t value of the first intersection point of the
-        ray with self, or None if no intersection occurs"""
-        hit = None
-        q = self.centre - ray.start
-        vDotQ = dot(ray.dir, q)
-        squareDiffs = dot(q, q) - self.radius * self.radius
-        discrim = vDotQ * vDotQ - squareDiffs
+
+        l = self.centre - ray.start # C-O
+        tca = dot(ray.dir, l) # tca
+
+        squareDiffs = dot(l, l) - self.radius * self.radius
+        discrim = tca * tca - squareDiffs
         if discrim >= 0:
-            root = sqrt(discrim)
-            t0 = (vDotQ - root)
-            t1 = (vDotQ + root)
+            thc = sqrt(discrim)
+            t0 = (tca - thc)
+            t1 = (tca + thc)
+            # we want the point in the positive direction
             if t0 < t1:
-                
+
                 result = t1
             else:
-                
+
                 result = t0
-            
+
             x = ray.start.x + ray.dir.dx * result
             y = ray.start.y + ray.dir.dy * result
             z = ray.start.z + ray.dir.dz * result
-
+        else:
+            x = 0
+            y = 0
+            z = 0
         return [x, y, z]
 
 
